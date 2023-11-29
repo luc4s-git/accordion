faf24170f36dce285ae91e46a9cebc48d95a0906
luc4s-git <lucassantos4332@hotmail.com>
2023-11-29 15:23:50 -0300

    removing old logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,12 @@ import SingleQuestion from './components/SingleQuestion';
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
-  // const questionMapping = question.map((q) => {
-  //   const { id, title, info } = q;
-  //   return (
-  //     <div key={id} className="question">
-  //       <header>
-  //         <h5>{title}</h5>
-  //         <button className="question-btn" onClick={() => console.log(info)}>
-  //           <FaPlus />
-  //         </button>
-  //       </header>
-  //     </div>
-  //   );
-  // });
 
   return (
     <main>
       <div className="container">
         <h1>Questions</h1>
         <SingleQuestion questions={questions} />
-        {/* {questionMapping} */}
       </div>
     </main>
   );
