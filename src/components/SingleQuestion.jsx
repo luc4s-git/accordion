6336adda9d006690fb8b16a876437b7ebc633835
luc4s-git <lucassantos4332@hotmail.com>
2023-11-29 15:23:15 -0300

    question iteration on single question component - css

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -4,13 +4,15 @@ export default function SingleQuestion({ questions }) {
   const questionsMapping = questions.map((question) => {
     const { id, title, info } = question;
     return (
-      <header key={id}>
-        <h5>{title}</h5>
-        <button className="question-btn">
-          <FaPlus></FaPlus>
-        </button>
-      </header>
+      <div className="question" key={id}>
+        <header>
+          <h5>{title}</h5>
+          <button className="question-btn">
+            <FaPlus></FaPlus>
+          </button>
+        </header>
+      </div>
     );
   });
-  return <div className="question">{questionsMapping}</div>;
+  return <>{questionsMapping}</>;
 }
