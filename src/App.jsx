eb2a7a19dc0c59e19c37e6670e6d78ccb5d953a4
luc4s-git <lucassantos4332@hotmail.com>
2023-11-29 15:05:47 -0300

    accordion initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+import questions from './data';
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [question, setQuestion] = useState(questions);
+  const questionMapping = question.map((q) => {
+    const { id, title, info } = q;
+    return (
+      <div key={id}>
+        <h1>{title}</h1>
+        <button className="question-btn"></button>
+      </div>
+    );
+  });
+
+  return (
+    <main>
+      <div className="container">{questionMapping}</div>
+    </main>
+  );
 };
 export default App;
