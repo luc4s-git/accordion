4e7e678eb5a0f9f49c38f002b352c8370b7ac15c
luc4s-git <lucassantos4332@hotmail.com>
2023-11-29 15:17:07 -0300

    single question component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa6';
-import questions from './data';
+import data from './data';
+import SingleQuestion from './components/SingleQuestion';
 
 const App = () => {
-  const [question, setQuestion] = useState(questions);
-  const questionMapping = question.map((q) => {
-    const { id, title, info } = q;
-    return (
-      <div key={id} className="question">
-        <header>
-          <h5>{title}</h5>
-          <button className="question-btn" onClick={() => console.log(info)}>
-            <FaPlus />
-          </button>
-        </header>
-      </div>
-    );
-  });
+  const [questions, setQuestions] = useState(data);
+  // const questionMapping = question.map((q) => {
+  //   const { id, title, info } = q;
+  //   return (
+  //     <div key={id} className="question">
+  //       <header>
+  //         <h5>{title}</h5>
+  //         <button className="question-btn" onClick={() => console.log(info)}>
+  //           <FaPlus />
+  //         </button>
+  //       </header>
+  //     </div>
+  //   );
+  // });
 
   return (
     <main>
       <div className="container">
         <h1>Questions</h1>
-        {questionMapping}
+        <SingleQuestion questions={questions} />
+        {/* {questionMapping} */}
       </div>
     </main>
   );
---
+++ src/components/SingleQuestion.jsx
@@ -0,0 +1,6 @@
+export default function SingleQuestion({ questions }) {
+  const questionsMapping = questions.map((question) => {
+    // return
+  });
+  return <h5>single question</h5>;
+}
