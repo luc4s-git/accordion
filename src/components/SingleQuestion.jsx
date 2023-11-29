59d1ae1f31b09ca411851bef175bf6da665456cd
luc4s-git <lucassantos4332@hotmail.com>
2023-11-29 15:33:09 -0300

    toggle question logic

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -1,16 +1,25 @@
-import { FaPlus } from 'react-icons/fa6';
+import { useState } from 'react';
+import { FaPlus, FaMinus } from 'react-icons/fa6';
 
 export default function SingleQuestion({ questions }) {
   const questionsMapping = questions.map((question) => {
     const { id, title, info } = question;
+    const [showInfo, setShowInfo] = useState(false);
+
     return (
       <div className="question" key={id}>
         <header>
           <h5>{title}</h5>
-          <button className="question-btn">
-            <FaPlus></FaPlus>
+          <button
+            className="question-btn"
+            onClick={() => {
+              setShowInfo(!showInfo);
+            }}
+          >
+            {showInfo ? <FaMinus /> : <FaPlus />}
           </button>
         </header>
+        <p>{showInfo && info}</p>
       </div>
     );
   });
